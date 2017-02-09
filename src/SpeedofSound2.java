89892f106185a383992fe0990c940297ae9da39c
ajvalenty <avalenty.aj@gmail.com>
2017-02-09 12:15:31 -0600

    added picture into Speed of Sound

--- src/SpeedofSound2.java
+++ src/SpeedofSound2.java
@@ -1,20 +1,44 @@
+import javax.swing.*;
+import java.awt.geom.*;
+import java.util.*;
+import java.awt.*;
 
 import java.awt.Color;
-
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.awt.Graphics;
+import javax.imageio.ImageIO;
 
-import javax.swing.JComponent;
+
+
+
+
 
 public class SpeedofSound2 extends UpdatableComponent 
 { 
 private double temperature;
 private double speed;
-    public SpeedofSound2(double temperature){
+private Image currentImage;
+private JPanel SpeedOfSoundPanel;
+    public SpeedofSound2(double temperature, JPanel SpeedOfSoundPanel){
     
     	super();
+    	this.SpeedOfSoundPanel=SpeedOfSoundPanel;
         this.temperature=temperature;
+        try
+        {
+            //both of these image methods work
+            currentImage = ImageIO.read(new File("TempGauge.png"));
+            //diceImage = Toolkit.getDefaultToolkit().getImage("dice.png");
+        } catch (IOException ex)
+        {
+            Logger.getLogger(SpeedOfSoundPanel.class.getName()).log(Level.WARNING,null,ex);
+        }
         
     }
+  
     public void setTemperature(double temperature){
       this.temperature=temperature;
     }
@@ -30,15 +54,21 @@ private double speed;
     }
 	public void paint(Graphics g) 
     {
-
+    	 Graphics2D g2d = (Graphics2D)g;
         g.setColor(Color.WHITE);
 
         g.fillRect(0, 0, getWidth(), getHeight());
         
         g.setColor(Color.BLACK);
+        g2d.drawImage(currentImage, (getWidth()/2)-150,(getHeight()/2)-150, 300,300, this);
         
+        g.drawString("First, you must know the Speed Of Sound Equation: velocity = 330.64 + .6(x degrees celsius)", (getWidth()/2)-100, (getHeight()/2)-50);
+        
+        g.drawString("Next, plug in your temperature, in this case: "+ temperature + " , to the equation: velocity = 330.64 + .6(" + temperature, getWidth()/2, (getHeight()/2)-25);
+        
+       
 
-        g.drawString("Speed of Sound at " + temperature + "degrees Celsius:" + String.valueOf(speed) + "m/s", getWidth()/2, getHeight()/2);
+        g.drawString("Speed of Sound at " + temperature + " degrees Celsius: " + String.valueOf(speed) + "m/s", getWidth()/2, getHeight()/2);
         
 
     }
