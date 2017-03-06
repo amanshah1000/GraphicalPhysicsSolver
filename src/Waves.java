d65c7d0d36294ff51044df0a550cd913c572828f
MarkMcconachie <Markm1206@gmail.com>
2017-03-06 11:48:31 -0600

    created trajectory

--- src/Waves.java
+++ src/Waves.java
@@ -41,7 +41,7 @@ public class Waves extends UpdatableComponent
         //resolution is how close each step is to the other the higher the resolution the higher the step.
         //resolution is not dependent on period
         // as you go too high in resolution you get weird artifacts. 64 doesnt give much artifacting
-        wavelength= wavelength/100;
+        wavelength= wavelength/100.0;
         double resolution =(wavelength/2)*64;
         double step = (2*Math.PI)/resolution;
         double maxSteps=(2*Math.PI)*periods;
@@ -77,6 +77,41 @@ public class Waves extends UpdatableComponent
 
     }
 
+
+    public void drawTrajectory(Graphics g,int initialX,int initialY,double velocity,double angle)
+    {
+        double functionX;
+        double functionY;
+        double xVelo = velocity*Math.cos(Math.toRadians(angle));
+        double yVelo = velocity*Math.sin(Math.toRadians(angle));
+        double range = (Math.pow(velocity,2) * Math.sin(2*Math.toRadians(angle)))/(9.81);
+        //the denominator of the step function is the number of points on the trajectory
+		double step = range/30;
+		//scale is used as a multiplier. so if you have a scale of X, that means X pixels = 1 meter
+		int scale = 10;
+        int currentX;
+        int currentY;
+
+        for (double i=0;i<=range;i=i+step)
+        {
+            functionX = i;
+            /*(i/xVelo)^2 is the time squared, there for when you sub that into the 2nd
+            *kinematic you end ump with yPosition as a function of xPosition
+            * therefore you can loop through all the xPositions and calculate there yPositions
+            * */
+
+            functionY = (-4.9*Math.pow((i/xVelo),2))+(Math.tan(Math.toRadians(angle))*i);
+            currentX =(int)(functionX*10);
+            currentY = (int)(functionY*10);
+
+
+            g.fillOval(currentX+initialX,initialY-currentY,4,4);
+        }
+
+
+
+    }
+
 	public void paint(Graphics g) 
     {
 
@@ -85,10 +120,28 @@ public class Waves extends UpdatableComponent
         g.fillRect(0, 0, getWidth(), getHeight());
         
         g.setColor(Color.BLACK);
+		//g.setColor(Color.RED);
         
 
         g.drawString("Wavelength is " + wavelength + "when Frequency is:" + String.valueOf(wavelength) + "m/s", getWidth()/2, getHeight()/2);
         drawWave(g,0,getHeight()/2,50,1,wavelength);
+        drawTrajectory(g,0,getHeight()/2,20,85);
+        /*
+		g.setColor(Color.BLACK);
+		drawTrajectory(g,0,getHeight()/2,20,40);
+		g.setColor(Color.RED);
+		drawTrajectory(g,0,getHeight()/2,20,35);
+		g.setColor(Color.BLACK);
+		drawTrajectory(g,0,getHeight()/2,20,30);
+		g.setColor(Color.RED);
+		drawTrajectory(g,0,getHeight()/2,20,25);
+		g.setColor(Color.BLACK);
+		drawTrajectory(g,0,getHeight()/2,20,20);
+		g.setColor(Color.RED);
+		drawTrajectory(g,0,getHeight()/2,20,15);
+		*/
+		//g.setColor(Color.BLACK);
+		//g.drawLine(0,getHeight()/2,getWidth(),getHeight()/2);
 
         
 
